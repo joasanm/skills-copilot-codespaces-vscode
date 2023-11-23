ee2e9b18def654e9cae59954ec95203bc6217e8a
joasanm <jsmjsm100@gmail.com>
2023-11-23 18:20:44 +0000

    Copilot second commit

--- member.js
+++ member.js
@@ -1,13 +1,19 @@
 function skillMember() {
-  return {
-    name: 'skillMember',
-    type: 'member',
-    path: '/skill/member',
-    component: () => import('@/views/skill/member'),
-    meta: {
-      title: '技能成员',
-      icon: 'table'
-    }
-  }
+    var member = document.getElementById("member");
+    var skill = document.getElementById("skill");
+    var project = document.getElementById("project");
+    var contact = document.getElementById("contact");
+    var memberContent = document.getElementById("member-content");
+    var skillContent = document.getElementById("skill-content");
+    var projectContent = document.getElementById("project-content");
+    var contactContent = document.getElementById("contact-content");
+    member.style.color = "#fff";
+    skill.style.color = "#000";
+    project.style.color = "#000";
+    contact.style.color = "#000";
+    memberContent.style.display = "block";
+    skillContent.style.display = "none";
+    projectContent.style.display = "none";
+    contactContent.style.display = "none";
 }
   
