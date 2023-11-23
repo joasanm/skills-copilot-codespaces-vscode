0dd0cef258988d2ca8bd642abfbba1409b64a006
joasanm <jsmjsm100@gmail.com>
2023-11-23 18:46:09 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,37 +1,60 @@
-// create web server
-// run node comments.js
-// open browser and go to http://localhost:8080
+// Create web server
+var express = require('express');
+var router = express.Router();
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var Comment = require('../models/Comment.js');
+var Post = require('../models/Post.js');
 
-var http = require('http');
-var url = require('url');
-
-var comments = [];
-
-var server = http.createServer(function(request, response) {
-  var parsedUrl = url.parse(request.url, true);
-  var path = parsedUrl.pathname;
-  var query = parsedUrl.query;
-  var method = request.method;
-
-  if (path === '/comments') {
-    if (method === 'GET') {
-      var string = JSON.stringify(comments);
-      response.statusCode = 200;
-      response.setHeader('Content-Type', 'application/json;charset=utf-8');
-      response.write(string);
-      response.end();
-    } else if (method === 'POST') {
-      var comment = query.comment;
-      comments.push(comment);
-      response.statusCode = 200;
-      response.end();
-    }
-  } else {
-    response.statusCode = 404;
-    response.end();
-  }
+// Create comment
+router.post('/', function(req, res, next) {
+    Comment.create(req.body, function (err, post) {
+        if (err) return next(err);
+        Post.findByIdAndUpdate(
+            req.body.post,
+            {$push: {"comments": post._id}},
+            {safe: true, upsert: true, new : true},
+            function(err, model) {
+                if(err){
+                    console.log(err);
+                    return res.send(err);
+                }
+                return res.json(model);
+            }
+        );
+    });
 });
 
-server.listen(8080, function() {
-  console.log('Server is running...');
+// Get all comments
+router.get('/', function(req, res, next) {
+    Comment.find(function (err, comments) {
+        if (err) return next(err);
+        res.json(comments);
+    });
 });
+
+// Get comment by id
+router.get('/:id', function(req, res, next) {
+    Comment.findById(req.params.id, function (err, comment) {
+        if (err) return next(err);
+        res.json(comment);
+    });
+});
+
+// Edit comment by id
+router.put('/:id', function(req, res, next) {
+    Comment.findByIdAndUpdate(req.params.id, req.body, function (err, comment) {
+        if (err) return next(err);
+        res.json(comment);
+    });
+});
+
+// Delete comment by id
+router.delete('/:id', function(req, res, next) {
+    Comment.findByIdAndRemove(req.params.id, req.body, function (err, comment) {
+        if (err) return next(err);
+        res.json(comment);
+    });
+});
+
+module.exports = router;
