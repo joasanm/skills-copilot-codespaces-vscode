efff6ddf4cc8214581ec4a3b45735b3063399198
joasanm <jsmjsm100@gmail.com>
2023-11-23 18:28:03 +0000

    Copilot second commit

--- member.js
+++ member.js
@@ -1,19 +1,6 @@
-function skillMember() {
-    var member = document.getElementById("member");
-    var skill = document.getElementById("skill");
-    var project = document.getElementById("project");
-    var contact = document.getElementById("contact");
-    var memberContent = document.getElementById("member-content");
-    var skillContent = document.getElementById("skill-content");
-    var projectContent = document.getElementById("project-content");
-    var contactContent = document.getElementById("contact-content");
-    member.style.color = "#fff";
-    skill.style.color = "#000";
-    project.style.color = "#000";
-    contact.style.color = "#000";
-    memberContent.style.display = "block";
-    skillContent.style.display = "none";
-    projectContent.style.display = "none";
-    contactContent.style.display = "none";
+function skillsMember() {
+    return {
+        restrict: 'E',
+        templateUrl: 'templates/member.html'
+    };
 }
-  
